9e9f82de9b6b03121aeb173a06e99481d4b29115
Heero.Law <heeroluo@gmail.com>
2016-12-29 10:11:53 +0800

    修复变量名错误

--- public/assets/common/xtpl/1.0/commands.mod.js
+++ public/assets/common/xtpl/1.0/commands.mod.js
@@ -2,5 +2,5 @@ exports.css =
 exports.js =
 exports.modjs = function(scope, option, buffer) {
 	var empty = '';
-	return options.fn ?  buffer.write(empty) : empty;
+	return option.fn ?  buffer.write(empty) : empty;
 };
