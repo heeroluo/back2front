b5f687bdbbc7d0894b68bfac6598e0ce174bc490
Heero.Law <heeroluo@gmail.com>
2018-06-06 16:26:22 +0800

    优化运行环境相关逻辑

--- config.js
+++ config.js
@@ -1,7 +1,27 @@
-let env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+const argvs = require('minimist')(process.argv.slice(2));
+
+let isProd;
+[argvs.env, process.env.NODE_ENV].some((item) => {
+	// 统一环境写法
+	item = {
+		'': 'local',
+		'development': 'dev',
+		'pre-release': 'pre',
+		'production': 'prod'
+	}[item] || item;
+
+	if (['local', 'dev', 'test', 'pre', 'prod'].indexOf(item) !== -1) {
+		isProd = !(item === 'local' || argvs.prod === false);
+		process.env.NODE_ENV = isProd ? 'production' : 'development';
+		process.env.BACK2FRONT_ENV = item;
+		return true;
+	}
+});
+
 
 module.exports = {
-	env: env,
+	// 环境
+	env: process.env.BACK2FRONT_ENV,
 
 	// 占用的端口
 	port: 3000,
@@ -11,9 +31,7 @@ module.exports = {
 
 	// 静态文件设置
 	static: {
-		maxAge: env === 'development' || env === 'test' ?
-			0 :
-			3 * 24 * 60 * 60 * 1000
+		maxAge: isProd ? 3 * 24 * 60 * 60 * 1000 : 0
 	},
 
 	// 前后端同构的XTemplate指令模块（路径相对于 lib/xtpl.js）
